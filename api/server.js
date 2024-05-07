154b7469e8db74d9fc875123822625d73befe8c4
Steve Schwab <schwabs@umich.edu>
2024-05-07 13:53:49 -0700

    import the custom users model containing the db access utility functions to access the mocked database

--- api/server.js
+++ api/server.js
@@ -1,4 +1,7 @@
 // import the express library to provide web server functionalities
 const express = require('express');
 
+// import the custom users model containing the database access utility functions
+const users = require('./users/model');
+
 module.exports = {};

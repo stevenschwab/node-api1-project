9dac7c8b9e6d5ed6ab1296fc4b783001223db851
Steve Schwab <schwabs@umich.edu>
2024-05-07 13:43:49 -0700

    add comments to the index file

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+// import the server instance from the server file
 const server = require('./api/server');
 
+// specify and define the port the server will listen for incoming connections on
 const port = 9000;
 
-// START SERVER HERE
+// start the server on the specified port and begin listening for incoming client connections
+// output a message to the console when the server starts successfully
 server.listen(port, () => {
     console.log(`server started on http://localhost:${port}`)
 })

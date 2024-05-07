bed7e02d700f57d2c9cc02d54e02387cadce336e
Steve Schwab <schwabs@umich.edu>
2024-05-07 16:52:54 -0700

    if there's an error in removing a user from the db, respond with a HTTP status code of 500 and return an error message

--- api/server.js
+++ api/server.js
@@ -60,6 +60,13 @@ server.get('/api/users/:id', async (req, res) => {
 
 server.delete('/api/users/:id', async (req, res) => {
     const { id } = req.params;
+    try {
+        throw new Error('error')
+    } catch (err) {
+        res.status(500).json({
+            message: "The user could not be removed"
+        })
+    }
 })
 
 // expose the server to other modules

887cea9369e5948ca93bc3ed3891c9dc99f1dd7f
Steve Schwab <schwabs@umich.edu>
2024-05-07 13:59:08 -0700

    add global middleware to the web server to parse incoming JSON payloads from the request body and make them accessible via req.body

--- api/server.js
+++ api/server.js
@@ -7,4 +7,7 @@ const users = require('./users/model');
 // create an instance of an express application representing the web server
 const server = express();
 
+// add global middleware to the web server to parse incoming JSON payloads from the request body
+server.use(express.json());
+
 module.exports = {};

a6c750035bfb70251aa0be406da556987b84fd67
Steve Schwab <schwabs@umich.edu>
2024-05-07 15:20:23 -0700

    respond with an HTTP status code of 500 and an error message if there's an error while saving the user to the db

--- api/server.js
+++ api/server.js
@@ -10,6 +10,17 @@ const server = express();
 // add global middleware to the web server to parse incoming JSON payloads from the request body
 server.use(express.json());
 
+// endpoints
+server.post('/api/users', (req, res) => {
+    const { name, bio } = req.body;
+    try {
+        throw new Error('error')
+    } catch (err) {
+        res.status(500).json({
+            message: "There was an error while saving the user to the database"
+        })
+    }
+})
 
 // expose the server to other modules
 module.exports = server;

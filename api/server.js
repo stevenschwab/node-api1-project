5da06e86766a4e0279daf1ee3ce94af5d0b556c1
Steve Schwab <schwabs@umich.edu>
2024-05-07 13:55:19 -0700

    create an instance of an express app representing the web server

--- api/server.js
+++ api/server.js
@@ -4,4 +4,7 @@ const express = require('express');
 // import the custom users model containing the database access utility functions
 const users = require('./users/model');
 
+// create an instance of an express application representing the web server
+const server = express();
+
 module.exports = {};

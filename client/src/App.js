29fadbf700247164f99e3845c852aaf9946bd54b
Steve Schwab <schwabs@umich.edu>
2024-05-08 10:54:02 -0700

    implement deleteUser function

--- client/src/App.js
+++ client/src/App.js
@@ -8,12 +8,22 @@ function App() {
   useEffect(() => {
     axios.get('http://localhost:9000/api/users')
       .then(res => {
-        setUsers(res.data)
+        setUsers(res.data);
       })
       .catch(err => {
         console.error('Error fetching users', err);
       });
   }, []);
+
+  const deleteUser = (id) => {
+    axios.delete(`http://localhost:9000/api/users/${id}`)
+      .then(res => {
+        setUsers(users.filter(user => user.id !== id));
+      })
+      .catch(err => {
+        console.error('Error deleting user', err);
+      })
+  }
 
   return (
     <div>
